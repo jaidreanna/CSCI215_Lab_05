f075f15c80aea6974eee0287656cc87a6234ea2c
jaidreanna <jaidreanna@gmail.com>
2018-10-09 18:23:06 -0400

    first commit

---
+++ lab_05/behavior.js
@@ -0,0 +1,82 @@
+console.clear();
+
+// ---------------------------------------------
+// Task 1
+
+var divNode = document.getElementByID('fiddle');
+// ---------------------------------------------
+
+
+// ---------------------------------------------
+// Task 2
+
+var pNode = document.createElement("P");
+var pTextnode = document.createTextNode("CSCI 215: Lab 5 Todo");
+
+TextNode.appendChild(pNode);
+
+pNode.appendChild(pTextnode);
+
+// ---------------------------------------------
+
+
+// ---------------------------------------------
+// Task 3
+
+var hrNode = document.createElement("hr");
+
+
+divNode.document.body.appendChild(hrNode);
+
+// ---------------------------------------------
+
+
+// ---------------------------------------------
+// Task 4
+
+
+
+var olNode = document.createElement("ol");
+
+var liNode1 = document.createElement("li");
+
+var liNode2 = document.createElement("li2");
+
+var liTextNode1 = document.createTextNode("Read Assignment");
+
+var liTextNode2 = document.createTextNode("Code Assignment");
+
+
+    liTextNode1.appendChild(liNode1);
+
+    liTextNode2.appendChild(liNode2);
+
+    liNode1.appendChild(olNode);
+
+    liNode2.appendChild(olNode);
+
+    olNode.appendChild(divNode);
+
+// ---------------------------------------------
+
+// ---------------------------------------------
+// Task 5
+
+var = imgNode;
+
+var srcAttr = document.getElementbyId("img");
+
+var srcAttr = document.createElement("src");
+
+
+scAttr.value = "http://munsellb.people.cofc.edu/img/prettypicture.jpg";
+
+imgNode.setAttributeNode(srcAttr);
+
+divNode.appendchild(imgNode);
+
+// ---------------------------------------------
+
+
+
+
